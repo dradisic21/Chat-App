6f6fa3e18b905f423b2369ad2eb0504f5cfa2a72
Dino Radišić <radisic.dino@gmail.com>
2023-01-17 10:30:11 +0100

    add sendMessage function

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import "./Chat.css";
 
 function Chat() {
+  const [message, setMessage] = useState("");
+
+  function sendMessage(e) {
+    console.log('usao u sendMessage')
+    e.preventDefault();
+    if (message && message.replace(/\s/g, "").length > 0) {
+      setMessage("");
+      console.log('poslana poruka');
+    }
+  }
   return (
     <div className="chat-container">
       <div className="chat-message__container">
@@ -9,11 +20,13 @@ function Chat() {
           <p>Chat room</p>
         </div>
         <div className="message-content"></div>
-        <form className="message-form__item">
+        <form className="message-form__item" onSubmit={sendMessage}>
           <input
             className="message-form__input"
             type="text"
             placeholder="Enter your Message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
           />
           <div className="message-form__button">
             <button className="button">Send</button>
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Messages() {
+    
     return (
         <div className="messages">
 
